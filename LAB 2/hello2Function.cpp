066d179aac7a735a3cc367cb52ff053aa12e62e5
Ssushiy <47434384+Ssushiy@users.noreply.github.com>
2020-01-15 16:09:58 -0800

    Add files via upload

---
+++ LAB 2/hello2Function.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Sushi on 1/15/20.
+//  Copyright © 2020 Sushi. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int hello2Function(string name2)
+{
+    cout << "hello World" << name2 << endl;
+    return 0;
+}
